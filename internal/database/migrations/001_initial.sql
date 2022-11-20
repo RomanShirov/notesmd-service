70b9abae887b20ebc2707683fea5cc437e30f905
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-20 22:12:21 +0300

    Edit migrations

--- internal/database/migrations/001_initial.sql
+++ internal/database/migrations/001_initial.sql
@@ -4,16 +4,18 @@ CREATE TABLE users
     id            SERIAL PRIMARY KEY,
     email         text UNIQUE NOT NULL,
     password_hash text        NOT NULL,
+    published     text[],
     created_at    timestamp DEFAULT NOW()
 );
 
 CREATE TABLE notes
 (
     id          SERIAL PRIMARY KEY,
-    uploader_id INT NOT NULL,
-    folder      text       NOT NULL,
-    title       text       NOT NULL,
+    uploader_id INT  NOT NULL,
+    folder      text NOT NULL,
+    title       text NOT NULL,
     data        text,
+    hash   text,
     updated_at  timestamp DEFAULT NOW()
 );
 
